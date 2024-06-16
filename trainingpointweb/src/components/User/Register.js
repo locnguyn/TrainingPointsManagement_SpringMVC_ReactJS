ced8444b7c2dcc1cf2eb2c8d30471d04cee16264
lokhue <2151013052loc@gmail.com>
2024-06-16 12:40:50 +0700

    commit

--- trainingpointweb/src/components/User/Register.js
+++ trainingpointweb/src/components/User/Register.js
@@ -3,6 +3,7 @@ import { Button, Container, Form } from "react-bootstrap";
 import APIs, { endpoints } from "../../configs/APIs";
 import { useNavigate } from "react-router-dom";
 import { MyUserContext } from "../../configs/Contexts";
+import Verify from "./Verify";
 
 const Register = () => {
   const fields = [
@@ -101,61 +102,78 @@ const Register = () => {
     }
   };
 
+  const RemainingField = () => (
+    <Form onSubmit={register}>
+      {fields.map((f) => (
+        <Form.Group key={f.field} className="mb-3" controlId={f.field}>
+          <Form.Label>{f.label}</Form.Label>
+          <Form.Control
+            onChange={(e) => Change(e, f.field)}
+            value={user[f.field]}
+            type={f.type}
+            placeholder={f.label}
+          />
+        </Form.Group>
+      ))}
+      {classes && classes.length > 0 && (
+        <>
+          <Form.Group className="mb-3">
+            <Form.Label>Lớp</Form.Label>
+            <Form.Select defaultValue="" required>
+              <option value="" disabled hidden>
+                Lớp
+              </option>
+              {classes.map((c) => (
+                <option value={c.id}>{c.name}</option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+        </>
+      )}
+      {faculties && faculties.length > 0 && (
+        <>
+          <Form.Group className="mb-3">
+            <Form.Label>Khoa</Form.Label>
+            <Form.Select defaultValue="" required>
+              <option value="" disabled hidden>
+                Khoa
+              </option>
+              {faculties.map((f) => (
+                <option value={f.id}>{f.name}</option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+        </>
+      )}
+
+      <Form.Group className="mb-3" controlId="avatar">
+        <Form.Control type="file" accept=".jpg,.png" ref={avatar} />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Button type="submit" value="primary">
+          Đăng Ký
+        </Button>
+      </Form.Group>
+    </Form>
+  );
+
+  const RenderStep = () => {
+    console.log(user);
+    switch (step) {
+      case 1:
+        return <Verify onSubmit={setUser} setStep={setStep} label="Email" placeholder="Email" endpoint="https://3e93-171-243-49-117.ngrok-free.app/TrainingPointApp/api/otp/create/" type="email"/>;
+      case 2:
+        return <Verify setStep={setStep} label={`Nhập mã OTP đã được gửi về email ${user["email"]}`} placeholder="Nhập OTP..." endpoint="https://3e93-171-243-49-117.ngrok-free.app/TrainingPointApp/api/otp/verify/" type="text" user={user}/>;
+      case 3:
+        return <RemainingField />;
+    }
+  }
+
+
   return (
     <Container className="custom-container">
       <h1 className="text-center">ĐĂNG KÝ</h1>
-      <Form onSubmit={register}>
-        {fields.map((f) => (
-          <Form.Group key={f.field} className="mb-3" controlId={f.field}>
-            <Form.Label>{f.label}</Form.Label>
-            <Form.Control
-              onChange={(e) => Change(e, f.field)}
-              value={user[f.field]}
-              type={f.type}
-              placeholder={f.label}
-            />
-          </Form.Group>
-        ))}
-        {classes && classes.length > 0 && (
-          <>
-            <Form.Group className="mb-3">
-              <Form.Label>Lớp</Form.Label>
-              <Form.Select defaultValue="" required>
-                <option value="" disabled hidden>
-                  Lớp
-                </option>
-                {classes.map((c) => (
-                  <option value={c.id}>{c.name}</option>
-                ))}
-              </Form.Select>
-            </Form.Group>
-          </>
-        )}
-        {faculties && faculties.length > 0 && (
-          <>
-            <Form.Group className="mb-3">
-              <Form.Label>Khoa</Form.Label>
-              <Form.Select defaultValue="" required>
-                <option value="" disabled hidden>
-                  Khoa
-                </option>
-                {faculties.map((f) => (
-                  <option value={f.id}>{f.name}</option>
-                ))}
-              </Form.Select>
-            </Form.Group>
-          </>
-        )}
-
-        <Form.Group className="mb-3" controlId="avatar">
-          <Form.Control type="file" accept=".jpg,.png" ref={avatar} />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Button type="submit" value="primary">
-            Đăng Ký
-          </Button>
-        </Form.Group>
-      </Form>
+      <RenderStep />
     </Container>
   );
 };
--- trainingpointweb/src/components/User/Verify.js
+++ trainingpointweb/src/components/User/Verify.js
@@ -1,29 +1,91 @@
+import axios from "axios";
+import { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import toast from "react-hot-toast";
 
-const Verify = () => {
-  const fields = [
-    {
-      field: "email",
-      type: "email",
-      label: "Email",
-    },
-  ];
+const Verify = ({ onSubmit = () => { }, setStep = () => { }, label, placeholder, endpoint, type, user }) => {
+  const [email, setEmail] = useState("");
+  const validateEmailFormat = (email) => {
+    const regex = /^\d{10}[a-z]+@ou\.edu\.vn$/
+    return regex.test(email);
+  };
+  const verifyEmail = async (e) => {
+    e.preventDefault();
+    if (type === "email") {
+      if (!validateEmailFormat(email)) {
+        toast.error("Đây không phải email trường cung cấp, vui lòng kiểm tra lại");
+      } else {
+        try {
+          if (type === "email") {
+            const res = await toast.promise(axios.post(endpoint, {
+              email: email
+            }), {
+              loading: `Đang gửi mã OTP đến email ${email}`,
+              success: `Đã gửi mã OTP đến email ${email}`,
+              // error: "Xảy ra lỗi!"
+            })
+
+            if (res.status === 201) {
+              onSubmit({ "email": email, "studentCode": email.substring(0, 10) });
+              setStep(2);
+            }
+          }
+
+        } catch (error) {
+          if(error.response.status === 400){
+            toast.error("Email đã được đăng ký!");
+          }
+          console.log(error);
+        }
+      }
+    } else {
+      try {
+        console.log(endpoint, {
+          email: user.email,
+          otp: email
+        })
+        const res = await toast.promise(axios.get(endpoint, {
+          headers: {
+            "ngrok-skip-browser-warning": ""
+          },
+          params: {
+            email: user.email,
+            otp: email
+          }
+        }), {
+          loading: "Đang xác thực mã OTP",
+          // success: "Đã xác thực mã OTP",
+          // error: "Xảy ra lỗi!"
+        })
+        console.log(res.data.message)
+        if (res.data == "Verified") {
+          console.log("verified");
+          toast.success("Verified");
+          setStep(3);
+        } else if (res.data.message == "Mã OTP đã hết hạn, vui lòng nhập lại email!") {
+          setStep(1);
+          toast.error("Otp expired");
+        } else if (res.data.message == "Otp not match"){
+          toast.error("Otp not match");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 
   return (
-    <Container className="custom-container">
-      <h1 className="text-center">ĐĂNG KÝ</h1>
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Email" required></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Button type="submit" variant="primary">
-            Tiếp Tục
-          </Button>
-        </Form.Group>
-      </Form>
-    </Container>
+    <Form onSubmit={verifyEmail}>
+      <Form.Group className="mb-3">
+        <Form.Label>{label}</Form.Label>
+        <Form.Control type={type} placeholder={placeholder} value={email} required onChange={(e) => setEmail(e.target.value)}></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Button type="submit" variant="primary">
+          Tiếp Tục
+        </Button>
+      </Form.Group>
+    </Form>
   );
 };
 
