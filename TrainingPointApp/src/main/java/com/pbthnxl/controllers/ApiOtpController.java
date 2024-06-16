91fbdc016c0c6cf12b384ad9b565e0ba74832d10
Hiếu <hieu@192.168.1.142>
2024-06-16 12:37:28 +0700

    update Otp API

--- TrainingPointApp/src/main/java/com/pbthnxl/controllers/ApiOtpController.java
+++ TrainingPointApp/src/main/java/com/pbthnxl/controllers/ApiOtpController.java
@@ -8,11 +8,13 @@ import com.pbthnxl.pojo.Otp;
 import com.pbthnxl.pojo.User;
 import com.pbthnxl.services.OtpService;
 import com.pbthnxl.services.UserService;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -21,6 +23,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -46,7 +49,7 @@ public class ApiOtpController {
 
     @CrossOrigin
     @PostMapping("/create/")
-    public ResponseEntity<String> sendEmailOtp(@RequestParam Map<String, String> params) {
+    public ResponseEntity<String> sendEmailOtp(@RequestBody Map<String, String> params) {
 
         User user = this.userService.getUserByEmail(params.get("email"));
 
@@ -54,32 +57,42 @@ public class ApiOtpController {
             return new ResponseEntity<>("Email Exists", HttpStatus.BAD_REQUEST);
         }
 
-        this.otpService.generateOTP(user, params.get("email"));
-
-        return new ResponseEntity<>("Success", HttpStatus.CREATED);
-    }
-
-    @CrossOrigin
-    @GetMapping("/verify/")
-    public ResponseEntity<String> verifyOtp(@RequestParam Map<String, String> params) {
-        Otp otp = this.otpService.findByEmail(params.get("email"));
-        if (otp == null) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-
-        String OTP = "";
-        if (params.containsKey("otp")) {
-            OTP = params.get("otp");
-            if (this.otpService.isOtpExpired(otp)) {
-                //OTP not expire
-                if (this.passwordEncoder.matches(OTP, otp.getOneTimePassword())) {
-                    return new ResponseEntity<>("Verified", HttpStatus.OK);
-                } else return new ResponseEntity<>("Otp not match", HttpStatus.OK); 
-            } else return new ResponseEntity<>("Otp expires", HttpStatus.OK); 
-
+        if (params.containsKey("email")) {
+            this.otpService.generateOTP(user, params.get("email"));
+            return new ResponseEntity<>("Success", HttpStatus.CREATED);
         }
 
         return new ResponseEntity<>("Failed", HttpStatus.BAD_REQUEST);
+
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/verify/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, String>> verifyOtp(@RequestBody Map<String, String> params) {
+        Map<String, String> response = new HashMap<>();
+
+        Otp otp = this.otpService.findByEmail(params.get("email"));
+        if (otp == null) {
+            response.put("message", "Otp not found");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+        if (params.containsKey("otp")) {
+            String OTP = params.get("otp");
+            if (this.otpService.isOtpExpired(otp)) {
+                response.put("message", "Otp expired");
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            } else if (this.passwordEncoder.matches(OTP, otp.getOneTimePassword())) {
+                response.put("message", "Verified");
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            } else {
+                response.put("message", "Otp not match");
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            }
+        }
+
+        response.put("message", "Failed");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
